2f4b1a2a99535012bdeb91242cabd93affecfccc
Nicolas KC <nicolas@kolnescassis.com>
2015-09-03 10:50:09 +0200

    added website title

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// Set website title
+app.set('title', 'Class Registration');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
